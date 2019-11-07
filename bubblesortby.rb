067b0d56cf609d822e4b54fb7ba26cf406002e63
Ed Sacasa <edward.sacasa@icloud.com>
2019-11-07 14:44:44 -0600

    bubble sort by

---
+++ bubblesortby.rb
@@ -0,0 +1,21 @@
+def bubble_sort_by(arr)
+  answer = []
+  i = 1
+
+  until arr.length == 0
+    while i <= arr.size - 1
+      if yield(arr[i - 1], arr[i]) >= 1
+        el = arr.slice!(i - 1)
+        arr.insert(i, el)
+      end
+      i += 1
+    end
+    answer << arr.pop
+    i = 1
+  end
+  p answer.reverse
+end
+
+bubble_sort_by(["hola", "hi", "hellopop", "hey", "qhuboman"]) do |left, right|
+  left.length - right.length
+end
