a033cb3ba4f0612d4966a494c269a9e4611bcb3d
elijahscriptdev <elijahobominuru@gmail.com>
2019-11-07 21:10:27 +0100

    bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+puts bubble_sort([1,3,5,2,8,9,2,4])
